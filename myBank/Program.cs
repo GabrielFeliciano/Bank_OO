0f212643ee5efdc113f786bc81893a8015c8e64c
Barbara Rezende <bccrezende@gmail.com>
2021-09-08 09:41:40 -0300

    Criação do projeto + classe conta corrente

---
+++ myBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace myBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaBarbara = new ContaCorrente();
+
+            contaDaBarbara.titular = "Barbara";
+            contaDaBarbara.agencia = 1234;
+            contaDaBarbara.conta = 789;
+            contaDaBarbara.saldo = 100.50;
+
+            ContaCorrente contaDoJoao = new ContaCorrente();
+
+            contaDoJoao.titular = "Joao";
+            contaDoJoao.agencia = 1234;
+            contaDoJoao.conta = 790; 
+            contaDoJoao.saldo = 120;
+
+            Console.WriteLine("O titular da conta é: " + contaDaBarbara.titular);
+            Console.WriteLine("O titular da conta é: " + contaDoJoao.titular);
+        }
+    }
+}
