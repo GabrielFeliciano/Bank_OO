4e02aba5c1d9c70c51c650f701dd6a4710bd2021
Barbara Rezende <bccrezende@gmail.com>
2021-09-09 09:38:20 -0300

    Criação do construtor da classe ContaCorrente

--- myBank/Program.cs
+++ myBank/Program.cs
@@ -6,22 +6,15 @@ namespace myBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente contaDaBarbara = new ContaCorrente();
+            ContaCorrente contaDaBarbara = new ContaCorrente("Barbara", 1234, 789, 100.50);
 
-            contaDaBarbara.titular = "Barbara";
-            contaDaBarbara.agencia = 1234;
-            contaDaBarbara.conta = 789;
-            contaDaBarbara.saldo = 100.50;
+            ContaCorrente contaDoJoao = new ContaCorrente("Joao", 1234, 790, 120);
 
-            ContaCorrente contaDoJoao = new ContaCorrente();
-
-            contaDoJoao.titular = "Joao";
-            contaDoJoao.agencia = 1234;
-            contaDoJoao.conta = 790; 
-            contaDoJoao.saldo = 120;
+            ContaCorrente contaDaMaria = new ContaCorrente("Maria", 1234, 791, 150);
 
             Console.WriteLine("O titular da conta é: " + contaDaBarbara.titular);
             Console.WriteLine("O titular da conta é: " + contaDoJoao.titular);
+            Console.WriteLine("O titular da conta é: " + contaDaMaria.titular);
         }
     }
 }
